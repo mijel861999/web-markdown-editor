b5932defffc02ecc3f05b2d18cb5015ca04338be
mijel <mijel.dev@gmail.com>
2022-10-21 22:51:32 -0500

    chore: agregando modal y librería de componentes

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Modal} from '@mui/material'
 import "./menu.scss";
 
 function Menu({ notes, setCurrentNote, setNotes }) {
-  const handleAddNote = () => {};
+	const [openModal, setOpenModal] = useState(false);
+
+
+	const handleAddNote = () => {
+		setOpenModal(true)
+	}
+
+	const handleModalClose = () => {}
 
   return (
     <div className="menu">
       <h1>Notas</h1>
-      <button onClick={handleAddNote}>Add</button>
+			<Button onClick={handleAddNote} variant="contained">Agregar</Button>
       <div className="notes">
         {notes.map((note) => (
           <h2 onClick={() => setCurrentNote(note)}>{note.title}</h2>
         ))}
       </div>
+
+			<Modal
+				open={openModal}
+				onClose={handleModalClose}
+				aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+			>
+				<Button onClick={() => setOpenModal(false)}>Cerrar</Button>
+			</Modal>
     </div>
   );
 }
